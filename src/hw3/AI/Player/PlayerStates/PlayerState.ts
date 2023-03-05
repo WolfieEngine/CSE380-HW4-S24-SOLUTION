c6ee29c3d11d6975ce0d917797522f3ba4134295
Peter Walsh <peter.t.walsh@stonybrook.edu>
2023-03-05 18:35:24 -0500

    Removed dead code from playerstate.

--- src/hw3/AI/Player/PlayerStates/PlayerState.ts
+++ src/hw3/AI/Player/PlayerStates/PlayerState.ts
@@ -31,6 +31,7 @@ export default abstract class PlayerState extends State {
     public override onEnter(options: Record<string, any>): void {}
     public override onExit(): Record<string, any> { return {}; }
     public override update(deltaT: number): void {
+
         // Adjust the angle the player is facing 
         this.parent.owner.rotation = this.parent.controller.rotation;
         // Move the player
@@ -51,101 +52,6 @@ export default abstract class PlayerState extends State {
 
         }
     }
-
-    public override handleInput(event: GameEvent): void {
-        // switch(event.type) {
-        //     case BattlerEvent.BATTLER_CHANGE: {
-        //         this.handleBattlerChange(event);
-        //         break;
-        //     }
-        //     case ItemEvent.INVENTORY_CHANGED: {
-        //         this.handleInventoryChange(event);
-        //         break;
-        //     }
-        //     case ItemEvent.CONSUMABLE_USED: {
-        //         this.handleConsumeItem(event);
-        //         break;
-        //     }
-        //     case BattlerEvent.HIT: {
-        //         this.handleBattlerHit(event);
-        //         break;  
-        //     }
-        //     default: {
-        //         throw new Error(`Unhandled event with type ${event.type} and data ${event.data} caught in PlayerState`);
-        //     }
-        // }
-    }
-
-    // protected handleBattlerChange(event: GameEvent): void {
-    //     let id: number = this.parent.battler.owner.id;
-    //     let curhp: number = this.parent.battler.health;
-    //     let maxhp: number = this.parent.battler.maxHealth;
-
-    //     // Update the players healthbar in the UI
-    //     this.emitter.fireEvent(HudEvent.HEALTH_CHANGE, {id: id, curhp: curhp, maxhp: maxhp});
-
-    //     // If the players battler has no health, the player is dead.
-    //     if (this.parent.battler.health <= 0) {
-    //         this.finished(PlayerStateType.DEAD);
-    //     }
-    // }
-    // protected handleInventoryChange(event: GameEvent): void {
-    //     let items: Item[] = event.data.get("inv");
-
-    //     // If the player doesn't have their held item in their inventory, clear the players held item
-    //     if (this.parent.item !== null && items.find((item: Item) => this.parent.item.owner.id === item.owner.id) === undefined) {
-    //         this.parent.item = null;
-    //     }
-    //     // If the player has items in their inventory, set the first item to be their held item
-    //     if (items.length !== 0) { 
-    //         this.parent.item = items.find((item: Item) => item)
-    //     }
-    //     // Update the inventory hud
-    //     this.emitter.fireEvent("inv", {items: items});
-    // }
-    // protected handleBattlerHit(event: GameEvent): void {
-    //     let item: Item = event.data.get("item");
-    //     switch(item.constructor) {
-    //         case Weapon: {
-    //             this.handleWeaponHit(event);
-    //             break;
-    //         }
-    //         default: {
-    //             throw new Error(`Attempting to handle item hit event for an unsupported item of class ${item.constructor}`);
-    //         }
-    //     }
-    // }
-    // protected handleWeaponHit(event: GameEvent): void {
-    //     let type: WeaponType = event.data.get("type");
-    //     switch(type.constructor) {
-    //         case LaserGun: {
-    //             if (event.data.get("userId") !== this.parent.owner.id) {
-    //                 this.parent.battler.health -= type.damage;
-    //             }
-    //             break;
-    //         }
-    //         case Wand: {
-    //             break;
-    //         }
-    //         default: {
-    //             throw new Error(`Attempting to handle weapon hit from unsupported weapon type ${type}.`);
-    //         }
-    //     }
-    // }
-    // protected handleConsumeItem(event: GameEvent): void {
-    //     let type: ConsumableType = event.data.get("type");
-    //     switch(type.constructor) {
-    //         case HealthPack: {
-    //             let effects = event.data.get("effects");
-    //             let health: number = effects.health;
-    //             this.parent.battler.health += health;
-    //             break;
-    //         }
-    //         default: {
-    //             throw new Error(`Attempting to handle consuming an unsupported consumable type ${type}`);
-    //         }
-    //     }
-    // }
 
 }
 
